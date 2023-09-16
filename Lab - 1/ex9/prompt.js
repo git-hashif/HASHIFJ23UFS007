130ad79b3e5ecb4bcca88437ba14e1711429f595
MOHAMED HASHIF KADERI <hashifkaderi@gmail.com>
2023-09-16 17:50:44 +0400

    cha

--- Lab - 1/ex9/prompt.js
+++
@@ -1,9 +0,0 @@
-window.onload = initAll;
-
-function initAll() {
-var ans = prompt("Are you sure you want to do that.?")
-if(ans){
-    alert("You Said " + ans);
-    document.write("You Said " +ans)
-}
-}
